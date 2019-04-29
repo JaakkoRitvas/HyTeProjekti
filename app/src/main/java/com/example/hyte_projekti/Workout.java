d7017efd2ca89c2c1686b2ecd7084f72526798ba
kuroshhu <mohammhu@metropolia.fi>
2019-04-29 11:09:45 +0300

    Workout

--- app/src/main/java/com/example/hyte_projekti/Workout.java
+++ app/src/main/java/com/example/hyte_projekti/Workout.java
@@ -1,4 +1,28 @@
 package com.example.hyte_projekti;
 
+import java.util.Date;
+
 public class Workout {
+    private String id;
+    private Date time;
+
+    public Workout() {
+        id = Integer.toString(hashCode());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
 }
