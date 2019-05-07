12942458234adee7ee4e586a0813c1a75d560da9
JaakkoRitvas <jaakko.ritvas@metropolia.fi>
2019-05-07 17:50:26 +0300

    Lisäystä

--- app/src/main/java/com/example/hyte_projekti/MainActivity.java
+++ app/src/main/java/com/example/hyte_projekti/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.hyte_projekti;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+
+import com.example.hyte_projekti.database.Workout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button lisaaTreeni = (Button) findViewById(R.id.lisays);
+        FloatingActionButton lisaaTreeni = (FloatingActionButton) findViewById(R.id.floatingActionButton);
     }
 
     public void onClick(View v) {
         Intent intent = new Intent(this, TreeninLisays.class);
         startActivity(intent);
     }
+    
 }
