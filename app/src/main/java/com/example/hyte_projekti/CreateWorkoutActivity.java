8f179cf6a6dcb215081729080272e8925be94dc9
MethodGearSeb <sebastian.sarviala@helsinki.fi>
2019-05-07 23:16:00 +0300

    eradicated some of the Finnish language code

--- app/src/main/java/com/example/hyte_projekti/CreateWorkoutActivity.java
+++ app/src/main/java/com/example/hyte_projekti/CreateWorkoutActivity.java
@@ -3,7 +3,6 @@ package com.example.hyte_projekti;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,34 +15,28 @@ import com.example.hyte_projekti.database.Workout;
 
 import java.util.Calendar;
 
-public class TreeninLisays extends AppCompatActivity {
+public class CreateWorkoutActivity extends AppCompatActivity {
 
-    EditText editDate;
-    EditText editTime;
-    EditText editTreeni;
-    Calendar currentDate;
-    Calendar currentTime;
-    Button valmis;
-
-    //TESTIKOODIA
-
-    private Handler mHandler = new Handler() ;
-    Thread thread;
-
+    private EditText editDate;
+    private EditText editTime;
+    private EditText editTreeni;
+    private Calendar currentDate;
+    private Calendar currentTime;
+    private Button buttonConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_treenin_lisays);
+        setContentView(R.layout.activity_create_workout);
 
 
         editDate = (EditText) findViewById(R.id.txtDate);
         editTime = (EditText) findViewById(R.id.txtTime);
         editTreeni = (EditText) findViewById(R.id.txtTreeni);
-        valmis = (Button) findViewById(R.id.button3);
+        buttonConfirm = (Button) findViewById(R.id.button3);
 
 
-        valmis.setOnClickListener(click);
+        buttonConfirm.setOnClickListener(click);
         editDate.setOnClickListener(click);
         editTime.setOnClickListener(click);
 
@@ -59,7 +52,7 @@ public class TreeninLisays extends AppCompatActivity {
                 int kuukaus = currentDate.get(Calendar.MONTH);
                 int paiva = currentDate.get(Calendar.DAY_OF_MONTH);
 
-                DatePickerDialog mDatePicker = new DatePickerDialog(TreeninLisays.this, new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog mDatePicker = new DatePickerDialog(CreateWorkoutActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int valittuVuosi, int valittuKuukaus, int valittuPaiva) {
                         editDate.setText(valittuPaiva + " - " + valittuKuukaus + " - " + valittuVuosi);
@@ -68,46 +61,41 @@ public class TreeninLisays extends AppCompatActivity {
                     }
                 }, vuosi, kuukaus, paiva);
                 mDatePicker.show();
-            }
-            else if(v == editTime) {
+            } else if (v == editTime) {
                 currentTime = Calendar.getInstance();
 
                 int tunti = currentTime.get(Calendar.HOUR_OF_DAY);
                 int minuutti = currentTime.get(Calendar.MINUTE);
 
-                TimePickerDialog mTimePicker = new TimePickerDialog(TreeninLisays.this, new TimePickerDialog.OnTimeSetListener() {
+                TimePickerDialog mTimePicker = new TimePickerDialog(CreateWorkoutActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int valittuTunti, int valittuMinuutti) {
                         editTime.setText(String.format("%02d:%02d", valittuTunti, valittuMinuutti));
                         //currentTime.set(valittuTunti, valittuMinuutti);
 
                     }
-                }, tunti, minuutti,true);
+                }, tunti, minuutti, true);
                 mTimePicker.show();
-                }
-            else if(v == valmis){
-                String date = editDate.getText().toString();
-                String time = editTime.getText().toString();
-                String name = editTreeni.getText().toString();
-
-                final Workout workout = new Workout();
-                workout.setDate(date);
-                workout.setTime(time);
-                workout.setName(name);
-                /*thread = new Thread(new Runnable() {
+            } else if (v == buttonConfirm) {
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
+                        String date = editDate.getText().toString();
+                        String time = editTime.getText().toString();
+                        String name = editTreeni.getText().toString();
+
+                        Workout workout = new Workout();
+                        workout.setDate(date);
+                        workout.setTime(time);
+                        workout.setName(name);
+
                         GlobalModel.getInstance().getDatabase().workoutDAO().insert(workout);
                     }
-                });
-                finish();*/
-                Intent intent = new Intent(TreeninLisays.this, MainActivity.class);
+                }).start();
+
+                Intent intent = new Intent(CreateWorkoutActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         }
     };
 }
-
-
-
-
--- app/src/main/java/com/example/hyte_projekti/MainActivity.java
+++ app/src/main/java/com/example/hyte_projekti/MainActivity.java
@@ -1,70 +1,61 @@
 package com.example.hyte_projekti;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-import android.arch.persistence.room.Room;
 import com.example.hyte_projekti.database.Workout;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lista;
-    /*private Handler mHandler = new Handler() ;
-    private Thread thread ;*/
+    ListView listWorkouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GlobalModel.getInstance().activateDatabase(getApplicationContext());
-        FloatingActionButton lisaaTreeni = (FloatingActionButton) findViewById(R.id.floatingActionButton);
-        List<Workout> workouts = GlobalModel.getInstance().getDatabase().workoutDAO().getPlannedWorkouts();
 
-        lista = (ListView) findViewById(R.id.lista);
+        listWorkouts = (ListView) findViewById(R.id.listWorkouts);
+        FloatingActionButton buttonCreateWorkout = (FloatingActionButton) findViewById(R.id.buttonCreateWorkout);
 
-        lista.setAdapter(new ArrayAdapter<Workout>(getApplicationContext(), android.R.layout.simple_list_item_1
-                , workouts));
+        buttonCreateWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateWorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                GlobalModel.getInstance().activateDatabase(getApplicationContext());
+                final List<Workout> workouts = GlobalModel.getInstance().getDatabase().workoutDAO().getWorkouts();
+
+                listWorkouts.setAdapter(new ArrayAdapter<>(
+                        getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        workouts
+                ));
+
+                listWorkouts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Workout workout = workouts.get(position);
+                        String subjectId = workout.getId();
+
+                        Intent intent = new Intent(MainActivity.this, ViewWorkoutActivity.class);
+                        intent.putExtra(GlobalModel.ID_TAG, subjectId);
+                        startActivity(intent);
+                    }
+                });
+            }
+        }).start();
     }
-    /*@Override
-    protected void onResume() {
-        super.onResume();
-
-            /*thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    List<Workout> workouts = GlobalModel.getInstance().getDatabase().workoutDAO().getPlannedWorkouts();
-
-                    lista = (ListView) findViewById(R.id.lista);
-
-                    lista.setAdapter(new ArrayAdapter<Workout>(getApplicationContext(), android.R.layout.simple_list_item_1
-                            , workouts));
-                }
-            });
-
-
-
-
-    }*/
-    public void onClick(View v) {
-
-        Intent intent = new Intent(MainActivity.this, TreeninLisays.class);
-
-        startActivity(intent);
-    }
-
-
-
 }
